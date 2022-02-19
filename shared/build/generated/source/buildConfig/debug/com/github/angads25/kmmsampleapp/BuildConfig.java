e57bcc3f0d6a4bbe09671e82582bcb169451e070
Sairam022 <b.sairam022@gmail.com>
2022-02-19 16:01:39 +0530

    Initial commit

---
+++ shared/build/generated/source/buildConfig/debug/com/github/angads25/kmmsampleapp/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.angads25.kmmsampleapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.github.angads25.kmmsampleapp";
+  public static final String BUILD_TYPE = "debug";
+}
